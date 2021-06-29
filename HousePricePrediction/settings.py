d8da51bdfcb0c8a9bb847139a3529d877ecf1186
Sarvajeet123 <sarvajeetnarkhede5072@gmail.com>
2021-06-29 23:31:18 +0530

    second commit

--- HousePricePrediction/settings.py
+++ HousePricePrediction/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-%$%w&8c=-+*g2t3nya+0zdya@#y3!$77od@facb*a!@xsu4s3l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'housepredict.herokuapp.com'
+]
 
 
 # Application definition
